f43413676853909bc7511950195876bc1369685d
Marina Barannikov <marina.barannikov@huggingface.co>
2024-06-26 11:38:06 +0200

    added support for calculating validation loss

--- lerobot/scripts/train_accelerate.py
+++ lerobot/scripts/train_accelerate.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import hydra
 import os
+import math
 
 import torch
 import torch.utils
@@ -19,12 +20,33 @@ from accelerate import Accelerator
 
 from omegaconf import OmegaConf, DictConfig
 
-# Create a directory to store the training checkpoint.
-output_directory = Path("/fsx/marina_barannikov/outputs/train/mixed_precision_test_accelerated_act")
-output_directory.mkdir(parents=True, exist_ok=True)
 
 pretrained_model_dir_name = "pretrained_model"
 training_state_file_name = "training_state.pth"
+
+
+def get_train_val_datasets(cfg, split_value=0.9):
+    # Load the last 10% of episodes of the dataset as a validation set.
+    # - Load full dataset
+    full_dataset = make_dataset(cfg, split="train")
+    # - Calculate train and val subsets
+    num_train_episodes = math.floor(full_dataset.num_episodes * split_value)
+    num_val_episodes = full_dataset.num_episodes - num_train_episodes
+    print(f"Number of episodes in full dataset: {full_dataset.num_episodes}")
+    print(f"Number of episodes in training dataset ({split_value*100}% subset): {num_train_episodes}")
+    print(f"Number of episodes in validation dataset ({split_value*100}% subset): {num_val_episodes}")
+    # - Get first frame index of the validation set
+    first_val_frame_index = full_dataset.episode_data_index["from"][num_train_episodes].item()
+    train_dataset = make_dataset(
+        cfg, split=f"train[:{first_val_frame_index}]"
+    )
+    val_dataset = make_dataset(
+        cfg, split=f"train[{first_val_frame_index}:]"
+    )
+    return train_dataset, val_dataset
+
+
+
 
 def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
     if out_dir is None:
@@ -48,16 +70,17 @@ def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
     set_global_seed(cfg.seed)
     accelerator.print(f"Global seed set to {cfg.seed}")
 
-    
-    offline_dataset = make_dataset(cfg)
-    accelerator.print(f"Dataset loaded with {len(offline_dataset)} samples")
+    train_dataset, val_dataset = get_train_val_datasets(cfg, split_value=0.9)
+
+    accelerator.print(f"Number of frames in training dataset (90% subset): {len(train_dataset)}")
+    accelerator.print(f"Number of frames in validation dataset (10% subset): {len(val_dataset)}")
 
     eval_env = None
     if cfg.training.eval_freq > 0:
         accelerator.print("make_env")
         eval_env = make_env(cfg)
 
-    policy = make_policy(cfg, dataset_stats=offline_dataset.stats)
+    policy = make_policy(cfg, dataset_stats=train_dataset.stats)
 
     optimizer, lr_scheduler = make_optimizer_and_scheduler(cfg, policy)
 
@@ -67,7 +90,7 @@ def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
     if cfg.training.get("drop_n_last_frames"):
         shuffle = False
         sampler = EpisodeAwareSampler(
-            offline_dataset.episode_data_index,
+            train_dataset.episode_data_index,
             drop_n_last_frames=cfg.training.drop_n_last_frames,
             shuffle=True,
         )
@@ -76,7 +99,7 @@ def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
         sampler = None
 
     train_dataloader = torch.utils.data.DataLoader(
-        offline_dataset,
+        train_dataset,
         num_workers=cfg.training.num_workers,
         batch_size=cfg.training.batch_size,
         shuffle=shuffle,
@@ -85,9 +108,18 @@ def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
         drop_last=False,
     )
 
+    val_dataloader = torch.utils.data.DataLoader(
+        val_dataset,
+        num_workers=cfg.training.num_workers,
+        batch_size=cfg.training.batch_size,
+        shuffle=False,
+        pin_memory=device != torch.device("cpu"),
+        drop_last=False,
+    )
 
-    policy, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
-        policy, optimizer, train_dataloader, lr_scheduler
+
+    policy, optimizer, train_dataloader, val_dataloader, lr_scheduler = accelerator.prepare(
+        policy, optimizer, train_dataloader, val_dataloader, lr_scheduler
     )
     policy.to(device)
 
@@ -112,16 +144,6 @@ def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
             accelerator.print("Start offline training on a fixed dataset")
 
         policy.train()
-        """
-        if resume_step>step and len(train_dataloader)%resume_step != 0 :
-            # We need to skip steps until we reach the resumed step
-            active_dataloader = accelerator.skip_first_batches(train_dataloader, resume_step)
-            accelerator.print(f"Skipping {resume_step} steps in the dataloader.")
-            step += resume_step
-        else:
-            # After the first iteration though, we need to go back to the original dataloader
-            active_dataloader = train_dataloader
-        """
 
         active_dataloader = train_dataloader
 
@@ -148,7 +170,7 @@ def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
                     if k != "loss":
                         accelerator.log({f"train/{k}": v}, step=step)
 
-            if step % cfg.training.save_freq == 0:
+            if step % cfg.training.save_freq == 0 and step > 0:
                 _num_digits = max(6, len(str(cfg.training.offline_steps + cfg.training.online_steps)))
                 step_identifier = f"{step:0{_num_digits}d}"
                 save_dir = out_dir / step_identifier
@@ -177,13 +199,28 @@ def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
                             continue
                         accelerator.log({f"eval/{k}": v}, step=step+1)
                     accelerator.print(f"Evaluated policy from process {accelerator.local_process_index}")
-                    
-            
+
             step += 1
 
             if step > cfg.training.offline_steps:
                 done = True
                 break
+                            
+            
+        policy.eval()
+        total_loss = 0
+
+        with torch.no_grad():
+            for batch in val_dataloader:
+                batch = {k: v.to(device, non_blocking=True) for k, v in batch.items()}
+                output_dict = policy.forward(batch)
+                loss = output_dict["loss"]
+                total_loss += loss.item()
+
+        average_loss = total_loss / len(val_dataloader)
+        accelerator.print(f"Validation loss: {average_loss:.3f}")
+        accelerator.log({"val/loss": average_loss}, step=step)
+            
 
     unwrapped_policy = accelerator.unwrap_model(policy)
     unwrapped_policy.save_pretrained(out_dir / "final")
@@ -200,3 +237,4 @@ def train_cli(cfg: dict):
 
 if __name__ == "__main__":
     train_cli()
+
