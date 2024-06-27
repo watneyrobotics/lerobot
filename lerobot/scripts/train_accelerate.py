2b9cf2fdcdcddab23e669ca7c4278e78d5074d55
Marina Barannikov <marina.barannikov@huggingface.co>
2024-06-27 13:47:09 +0000

    log eval info to WB

--- lerobot/scripts/train_accelerate.py
+++ lerobot/scripts/train_accelerate.py
@@ -1,25 +1,20 @@
+import math
+import os
 from pathlib import Path
 
 import hydra
-import os
-import math
-
 import torch
 import torch.utils
-
-from lerobot.common.utils.utils import set_global_seed, format_big_number
-from lerobot.common.datasets.factory import make_dataset
-from lerobot.common.policies.factory import make_policy
-from lerobot.common.envs.factory import make_env
-
-from lerobot.scripts.train import make_optimizer_and_scheduler
-from lerobot.common.datasets.sampler import EpisodeAwareSampler
-from lerobot.scripts.eval import eval_policy
-
 from accelerate import Accelerator
+from omegaconf import DictConfig, OmegaConf
 
-from omegaconf import OmegaConf, DictConfig
-
+from lerobot.common.datasets.factory import make_dataset
+from lerobot.common.datasets.sampler import EpisodeAwareSampler
+from lerobot.common.envs.factory import make_env
+from lerobot.common.policies.factory import make_policy
+from lerobot.common.utils.utils import set_global_seed
+from lerobot.scripts.eval import eval_policy
+from lerobot.scripts.train import make_optimizer_and_scheduler
 
 pretrained_model_dir_name = "pretrained_model"
 training_state_file_name = "training_state.pth"
@@ -37,15 +32,9 @@ def get_train_val_datasets(cfg, split_value=0.9):
     print(f"Number of episodes in validation dataset ({split_value*100}% subset): {num_val_episodes}")
     # - Get first frame index of the validation set
     first_val_frame_index = full_dataset.episode_data_index["from"][num_train_episodes].item()
-    train_dataset = make_dataset(
-        cfg, split=f"train[:{first_val_frame_index}]"
-    )
-    val_dataset = make_dataset(
-        cfg, split=f"train[{first_val_frame_index}:]"
-    )
+    train_dataset = make_dataset(cfg, split=f"train[:{first_val_frame_index}]")
+    val_dataset = make_dataset(cfg, split=f"train[{first_val_frame_index}:]")
     return train_dataset, val_dataset
-
-
 
 
 def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
@@ -53,15 +42,21 @@ def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
         raise NotImplementedError()
     if job_name is None:
         raise NotImplementedError()
-    
-    out_dir=Path(out_dir)
+
+    out_dir = Path(out_dir)
     out_dir.mkdir(parents=True, exist_ok=True)
 
     accelerator = Accelerator(log_with="wandb")
 
     accelerator.init_trackers(
         project_name="lerobot",
-        init_kwargs={"wandb": {"name":job_name, "job_type": "train", "config": OmegaConf.to_container(cfg, resolve=True)}}
+        init_kwargs={
+            "wandb": {
+                "name": job_name,
+                "job_type": "train",
+                "config": OmegaConf.to_container(cfg, resolve=True),
+            }
+        },
     )
     # Check device is available
     device = accelerator.device
@@ -117,7 +112,6 @@ def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
         drop_last=False,
     )
 
-
     policy, optimizer, train_dataloader, val_dataloader, lr_scheduler = accelerator.prepare(
         policy, optimizer, train_dataloader, val_dataloader, lr_scheduler
     )
@@ -125,16 +119,16 @@ def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
 
     step = 0
 
-    if cfg.resume==True or cfg.resume=="true":
+    if cfg.resume == True or cfg.resume == "true":
         subfolders = [p for p in out_dir.iterdir() if p.is_dir()]
         latest_subfolder = max(subfolders, key=lambda p: os.path.getctime(str(p)))
         accelerator.print(f"Resuming from {latest_subfolder}")
-        resume_step=int(latest_subfolder.name) + 1
+        resume_step = int(latest_subfolder.name) + 1
         accelerator.print(f"Resumed from step: {resume_step}")
         accelerator.load_state(latest_subfolder)
     else:
         accelerator.print("Starting from scratch")
-        resume_step=0
+        resume_step = 0
 
     step = resume_step
 
@@ -155,7 +149,7 @@ def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
             accelerator.backward(loss)
 
             grad_norm = accelerator.clip_grad_norm_(policy.parameters(), cfg.training.grad_clip_norm)
-            
+
             optimizer.step()
             optimizer.zero_grad()
 
@@ -177,7 +171,7 @@ def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
                 accelerator.print(f"Saving state to {save_dir}")
                 accelerator.save_state(save_dir)
                 OmegaConf.save(cfg, save_dir / "config.yaml")
-            
+
             if cfg.training.eval_freq > 0 and step % cfg.training.eval_freq == 0:
                 accelerator.print(f"Evaluating policy from process {accelerator.local_process_index}")
                 with torch.no_grad():
@@ -186,18 +180,18 @@ def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
                         eval_env,
                         accelerator.unwrap_model(policy),
                         cfg.eval.n_episodes,
-                        videos_dir=out_dir/ "eval" / f"videos_step_{step}",
+                        videos_dir=out_dir / "eval" / f"videos_step_{step}",
                         max_episodes_rendered=4,
                         enable_progbar=True,
                         start_seed=cfg.seed,
                     )
 
-                    for k, v in eval_info.items():
+                    for k, v in eval_info["aggregated"].items():
                         accelerator.print(f"eval/{k}: {v} at {step+1}")
                         if not isinstance(v, (int, float)):
                             accelerator.print(f"Skipping {k} from logging because it is not a scalar")
                             continue
-                        accelerator.log({f"eval/{k}": v}, step=step+1)
+                        accelerator.log({f"eval/{k}": v}, step=step + 1)
                     accelerator.print(f"Evaluated policy from process {accelerator.local_process_index}")
 
             step += 1
@@ -205,8 +199,7 @@ def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
             if step > cfg.training.offline_steps:
                 done = True
                 break
-                            
-            
+
         policy.eval()
 
         total_loss = 0
@@ -220,13 +213,13 @@ def train(cfg: DictConfig, job_name, out_dir, resume_checkpoint=None):
         average_loss = total_loss / len(val_dataloader)
         accelerator.print(f"Validation loss: {average_loss:.3f}")
         accelerator.log({"val/loss": average_loss}, step=step)
-            
 
     unwrapped_policy = accelerator.unwrap_model(policy)
     unwrapped_policy.save_pretrained(out_dir / "final")
     accelerator.print("Finished offline training")
     accelerator.end_training()
-    
+
+
 @hydra.main(version_base="1.2", config_name="default", config_path="../configs")
 def train_cli(cfg: dict):
     train(
@@ -235,6 +228,6 @@ def train_cli(cfg: dict):
         out_dir=hydra.core.hydra_config.HydraConfig.get().run.dir,
     )
 
+
 if __name__ == "__main__":
     train_cli()
-
