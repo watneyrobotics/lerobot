6f33840972276b2bc24451d2ebf0793fdaea6245
Marina Barannikov <marina.barannikov@huggingface.co>
2024-07-31 18:18:46 +0000

    update to torch squeeze

--- dev/compute_mse_l1_validation_loss.py
+++ dev/compute_mse_l1_validation_loss.py
@@ -1,16 +1,14 @@
-import math
-import torch
-import torch.functional as F
-
-from pathlib import Path
-import os
 import json
+import math
+import os
+from pathlib import Path
 
+import torch
 
 from lerobot.common.datasets.factory import make_dataset
-from lerobot.common.utils.utils import init_hydra_config
-from lerobot.common.policies.factory import make_policy
 from lerobot.common.policies.diffusion.modeling_diffusion import DiffusionPolicy
+from lerobot.common.policies.factory import make_policy
+from lerobot.common.utils.utils import init_hydra_config
 
 
 def get_train_val_datasets(cfg, split_value=0.9):
@@ -27,6 +25,7 @@ def get_train_val_datasets(cfg, split_value=0.9):
     first_val_frame_index = full_dataset.episode_data_index["from"][num_train_episodes].item()
     val_dataset = make_dataset(cfg, split=f"train[{first_val_frame_index}:]")
     return val_dataset
+
 
 def evaluate_checkpoint(policy, validation_loader, device):
     policy.eval()
@@ -47,11 +46,11 @@ def evaluate_checkpoint(policy, validation_loader, device):
         with torch.no_grad():
             policy.reset()
             predicted_actions = policy.select_action(observation)
+            print("Calculating MSE and L1 losses")
             mse_losses.append(mse_loss(predicted_actions, actions).item())
             l1_losses.append(l1_loss(predicted_actions, actions).item())
 
     return sum(mse_losses) / len(mse_losses), sum(l1_losses) / len(l1_losses)
-
 
 
 def main(hydra_cfg, root_dir, output_file):
@@ -64,21 +63,26 @@ def main(hydra_cfg, root_dir, output_file):
     for subdir in os.listdir(root_dir):
         if subdir.isdigit():  # Check if the subdirectory name is numerical
             subdir_path = os.path.join(root_dir, subdir)
-            checkpoint_path = os.path.join(subdir_path, 'pretrained_model/model.safetensors')
+            checkpoint_path = os.path.join(subdir_path, "model.safetensors")
             if os.path.isfile(checkpoint_path):
-                policy = make_policy(hydra_cfg=hydra_cfg, pretrained_policy_name_or_path=subdir_path + "/pretrained_model")
+                print(f"Loading checkpoint from {checkpoint_path}")
+                policy = make_policy(hydra_cfg=hydra_cfg, pretrained_policy_name_or_path=subdir_path)
+                print("Evaluating checkpoint")
                 mse, l1 = evaluate_checkpoint(policy, validation_loader, device)
-                results.append({'checkpoint': subdir_path, 'l1_loss': l1, 'step': int(subdir), 'mse_loss': mse})
+                results.append(
+                    {"checkpoint": subdir_path, "l1_loss": l1, "step": int(subdir), "mse_loss": mse}
+                )
 
-    with open(output_file, 'w') as f:
+    with open(output_file, "w") as f:
         json.dump(results, f, indent=4)
 
     return
 
+
 if __name__ == "__main__":
-    policy_path = Path('/Users/mbar/Desktop/projects/huggingface/lerobot/outputs/test_mps_operations/checkpoints/100100/pretrained_model')
+    policy_path = Path("/fsx/marina_barannikov/outputs/train/compare_val_loss/pusht_84/010000")
     cfg = init_hydra_config(str(policy_path / "config.yaml"))
     cfg.training.delta_timestamps["observation.image"] = [0]
     cfg.training.delta_timestamps["action"] = [i / 10 for i in range(8)]
     cfg.training.delta_timestamps["observation.state"] = [0]
-    main(cfg, '/Users/mbar/Desktop/projects/huggingface/lerobot/outputs/test_mps_operations/checkpoints', 'dev/test.json')
+    main(cfg, "/fsx/marina_barannikov/outputs/train/compare_val_loss/pusht_84", "dev/test.json")
